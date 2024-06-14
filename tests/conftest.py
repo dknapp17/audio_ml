2a946cfcb4c1f68fcc110bad0a6813ec1d2b5a03
dknapp17 <dsknapp17@gmail.com>
2024-06-14 11:06:12 -0400

    update pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,4 @@ import pytest
 
 @pytest.fixture()
 def test_file():
-    return './freesound\\247150\\overall quality of single note - trumpet - D#5.wav'
+    return './tests/overall quality of single note - trumpet - D#5.wav'
