5430fbf47aa40747a49ab510533637f23beed1a3
dknapp17 <dsknapp17@gmail.com>
2024-12-20 18:52:16 +0000

    modify main page

--- audio_ML/audio_model/audio_classifier.py
+++ audio_ML/audio_model/audio_classifier.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from torch.nn import init
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="Instrument Classifier",
+    page_icon="🎵",
 )
 
 st.write("# Instrument Prediction! 👋")
